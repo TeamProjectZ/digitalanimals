b42c1461310f4037e73358da3470f7d8140027f5
AntonMiracle <b.anton.m.1986@gmail.com>
2016-05-17 09:36:28 +0200

    Test 1st commit

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+import java.util.GregorianCalendar;
+
+/** 
+ * Project: DigitalAnimals
+ * Test.java  
+ */
+
+/**
+ * @author Bondarenko Anton Michailovich
+ * @version 1.0
+ *
+ */
+public class Test {
+	public static LocalDateTime timePoint = LocalDateTime.now();  
+	//testtesttest
+		public static void main(String[] args){
+			GregorianCalendar cc = new GregorianCalendar();
+			System.out.println("Hello World");
+			System.out.println(timePoint);
+//			cc.setGregorianChange(date);
+//			System.out.println(cc.isSet());
+			
+		}
+}
