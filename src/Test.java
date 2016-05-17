ed3b6bad6e6b75c633d577e08a436303fce0a9ad
AntonMiracle <b.anton.m.1986@gmail.com>
2016-05-17 09:45:19 +0200

    4d test com

--- src/Test.java
+++ src/Test.java
@@ -13,7 +13,7 @@ import java.util.GregorianCalendar;
  */
 public class Test {
 	public static LocalDateTime timePoint = LocalDateTime.now();  
-	//testtesttestddddddddd
+	//testtesttestdddddddddsdsd
 		public static void main(String[] args){
 			GregorianCalendar cc = new GregorianCalendar();
 			System.out.println("Hello World");
